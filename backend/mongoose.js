53f95da210570bf9ca9c898a66510c08b5fe66fc
Xander-AJ <xander.kamau7@gmail.com>
2023-10-03 12:28:57 +0300

    secured my credentials and set them in an environment

--- backend/mongoose.js
+++ backend/mongoose.js
@@ -1,15 +1,11 @@
 const mongoose = require('mongoose');
+require('dotenv').config();  // Load environment variables from .env file
 
-// MongoDB connection URL
-const mongoURI = 'mongodb+srv://johnalexander:hE9nX7dEVFJZC9jR@ip-2-moringa.zcwmrtp.mongodb.net/?retryWrites=true&w=majority';
-
-// Connect to MongoDB
-mongoose.connect(mongoURI, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-// Check MongoDB connection
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => {
